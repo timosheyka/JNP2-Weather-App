dccd4e621c54d5a4100be5436c4669e3a348f1f9
timosheyka <timosheyka@DESKTOP-N15SSSN>
2023-09-14 15:52:42 +0300

    provide your keys

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const WEATHER_API = "https://api.weatherapi.com/v1";
-export const WEATHER_API_KEY = "d4a2012b3c774029a61205328230606";
+export const WEATHER_API_KEY = "";
 
 export const TENOR_API = "https://tenor.googleapis.com/v2";
-export const TENOR_API_KEY = "AIzaSyB8l7I0ksVVcMWRO4Sx2l7-lZNi9t16xpM";
+export const TENOR_API_KEY = "";
